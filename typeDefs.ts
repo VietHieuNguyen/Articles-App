a10aad87ff5e760c658df0aab445d5ad3d39c0d7
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-28 11:23:19 +0700

    B43 1 2.4.Lấy ra danh sách bài viết

--- typeDefs.ts
+++ typeDefs.ts
@@ -1,7 +1,15 @@
 
 //GraphQL
 export  const typeDefs = `#graphql
+
+  type Article {
+    id: ID,
+    title: String,
+    avatar: String,
+    description: String
+  }
   type Query {
     hello: String
+    getListArticle: [Article]
   }
 `;
