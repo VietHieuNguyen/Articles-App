ca49fcc73f317f05025a663bc69cb39d218e6043
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-30 00:46:11 +0700

    B43 1 3. Danh mục

--- resolvers.ts
+++ resolvers.ts
@@ -1,4 +1,5 @@
 import Article from "./models/article.model"
+import Category from "./models/category.model"
 
 export const resolvers = {
     Query: {
@@ -18,7 +19,21 @@ export const resolvers = {
           deleted: false
         })
         return articles
-      }
+      },
+      getListCategory: async ()=>{
+        const categories = await Category.find({
+          deleted: false
+        })
+        return categories
+      },
+      getCategory:async(_: any, args:any)=>{
+          const {id} = args;
+          const category = await Category.findOne({
+            _id: id,
+          deleted: false
+        })
+        return category
+      },
     },
     Mutation:{
       createArticle: async (_:any, args: any)=>{
@@ -50,6 +65,37 @@ export const resolvers = {
         })
         return record
 
-      }
+      },
+      createCategory: async (_:any, args: any)=>{
+        const {category} = args;
+
+        const records = new Category(category);
+        await records.save();
+
+        return records;
+        
+      },
+      updateCategory: async (_: any, args: any) => {
+        const { id, category } = args;
+         await Category.updateOne({
+          _id: id
+        }, category)
+        const record = await Category.findOne({
+          _id: id
+        })
+        return record
+
+      },
+      deleteCategory: async (_:any, args: any)=>{
+        const {id} = args;
+        await Category.updateOne({
+          _id: id,
+        },{
+          deleted: true,
+          deletedAt: Date.now()
+        })
+        return "Thành công"
+      },
+      
     }
   }
--- typeDefs.ts
+++ typeDefs.ts
@@ -8,19 +8,40 @@ export  const typeDefs = `#graphql
     avatar: String,
     description: String
   }
+  type Category {
+    id: ID,
+    title: String,
+    avatar: String,
+
+  }
   type Query {
     hello: String
     getListArticle: [Article]
     getArticle(id: ID): Article
+
+    getListCategory: [Category]
+    getCategory(id: ID): Category
+
+
   }
   input ArticleInput{
     title: String,
     avatar: String,
     description: String
   }
+  input CategoryInput{
+    title: String,
+    avatar: String
+  }
   type Mutation{  #thêm sửa xóa
     createArticle(article: ArticleInput): Article
     deleteArticle(id: ID): String
     updateArticle(id: ID, article: ArticleInput): Article
+
+    createCategory(category: CategoryInput): Category
+    updateCategory(id: ID, category: CategoryInput): Category
+    deleteCategory(id: ID): String
+
+
   }
 `;
